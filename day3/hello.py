9a62ac6713e604421101e723a501f435c46dc4df
ultrapowerAU <leichen070@gmail.com>
2017-09-02 09:00:43 +0800

    第一个python程序

---
+++ day3/hello.py
@@ -0,0 +1,24 @@
+#-*- coding:utf-8 -*-
+'''#coding=utf-8'''
+#print("abc") 单行注释
+print("abc")
+'''
+print("abc")
+'''
+"""
+print("abc")
+print("abc")
+"""
+'''
+python 注释
+＃单行注释
+‘’‘多行注释’‘’
+
+用python2默认不是utf-8编码的，当代码中有中文时，需要在第一行声明编码
+#coding=utf-8 或
+#-*- coding:utf-8 -*- 
+python 默认使用python2
+python3 使用python3 macos 默认没有 ubuntu有
+
+ipython 可以在启动解释器的情况下使用linux命
+'''
