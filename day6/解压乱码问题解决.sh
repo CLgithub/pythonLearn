493cb062158afa7a0c8fe6e9cb8d14189bd7a318
ultrapowerAU <leichen070@gmail.com>
2017-09-14 21:22:36 +0800

    匿名函数1

---
+++ day6/解压乱码问题解决.sh
@@ -0,0 +1,4 @@
+ LANG=C 7za x 第八天.zip
+ convmv -f GBK -t utf8 --notest -r .
+
+ #https://www.zhihu.com/question/20523036
---
+++ day8/01-匿名函数.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+'''
+匿名函数
+  定义
+  lambda 参数:函数体
+'''
+#定义一个匿名函数
+lam1=lambda x,y:x+y #匿名函数函数体不需要写return
+#调用匿名函数
+a=lam1(3,4)
+print(a)
+
+lam2=lambda x:print(x)
+b=lam2(4)
