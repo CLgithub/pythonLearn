a5b1f98c55a20da34638e89bbdaf20a5e37225bf
L <leichen070@gmail.com>
2018-01-03 23:04:16 +0800

    字符串常见操作

--- day8/08-字符串常用操作.py
+++ day8/08-字符串常用操作.py
@@ -3,7 +3,7 @@
 常用字符串操作
     1
 '''
-str1="012345 789abcdef abc a"
+str1="a12345 789abcdef abc a"
 print("str=%s"%str1)
 print("-"*30)
 # 查找
@@ -27,4 +27,52 @@ print("str1.count(x)=%s"%str1.count("x"))
 print("-"*30)
 print("str1.replace(a,x)=%s"%str1.replace("a","x"))
 print("str1.replace(a,x,1)=%s"%str1.replace("a","x",2))
+
+# 切割
+# str.split(a) 按a切割str
+print("-"*30)
+print("str1.split(a)=%s"%str1.split("a"))
+#print("str1.partition(a)=%s"%str1.partition(""))
+
+# 首字母大写
+print("-"*30)
+print("str1.capitalize()=%s"%str1.capitalize())
+# 所有单词首字母大写
+print("str1.title()=%s"%str1.title())
+
+# 判断是非以某字符串开头或结尾
+print("-"*30)
+print("str1.startswith(a)=%s"%str1.startswith("a"))
+print("str1.endswith(a)=%s"%str1.endswith("a"))
+
+# 将字符串全部切换成大写或小写
+print("-"*30)
+print("str1.upper()=%s"%str1.upper())
+print("str1.upper().lower()=%s"%str1.upper().lower())
+
+# 左右对齐
+print("-"*30)
+print("str1.ljust(30)|=%s%s"%(str1.ljust(30),"|"))
+print("str1.ljust(3)|=%s%s"%(str1.ljust(3),"|"))
+print("str1.rjust(30)=%s"%(str1.rjust(30)))
+print("str1.rjust(3)=%s"%(str1.rjust(3)))
+print("|str1.center(30)|=|%s|"%(str1.center(30)))
+str2="ab"
+print("|str2.center(3)|=|%s|"%(str2.center(3)))
+
+# 去掉空格
+print("-"*30)
+print("|str1.center(40).lstrip()|=|%s|"%(str1.center(40).lstrip()))
+print("|str1.center(40).rstrip()|=|%s|"%(str1.center(40).rstrip()))
+print("|str1.center(40).strip()|=|%s|"%(str1.center(40).strip()))
+
+
+
+
+
+
+
+
+
+
 
