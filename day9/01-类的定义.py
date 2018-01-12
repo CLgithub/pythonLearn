a0c54f7fe3dab7bb3d5170cdd11d350b87088a33
Lmbp <leichen070@gmail.com>
2018-01-12 20:39:05 +0800

    ”类的定义就属性的添加及访问“

--- day9/01-类的定义.py
+++ day9/01-类的定义.py
@@ -4,7 +4,7 @@
 class 类名:
     属性
     方法
-    def 方法名(self):
+    def 方法名(self):     self相当于java中的this
         方法体
 根据类创建对象
 类名()
@@ -15,23 +15,27 @@ class 类名:
 class Cat:
     name="tom"
     def eat(self):
-        print("🐱在吃🐟")
+        print("%s🐱在吃🐟"%self.name)
     def drink(self):
-        print("🐱在和🍶")
-    def introduce(self):
-        #print("%s的年龄是%d"%(Cat.name,cat1.age))
-        print("%s的年龄是%d,Cat的age=%d"%(Cat.name,cat1.age,Cat.age))
+        print("%s🐱在和🍶"%self.name)
 
+    def introduce2(self):
+        print("------------%s-----------"%self)
+        #print("%s的年龄是%d"%(Cat.name,cat1.age))
+        print("a=%d"%(cat1.a)) #访问cat1的a
+        print("a=%d"%(self.a)) # 访问每个对象自己的a，没有就访问类的，类也没有a就报错
 #创建一个对象
 cat1=Cat()
 cat2=Cat()
-cat1.age=19 #给cat1添加属性
-cat2.age=20 #给cat2添加属性
-Cat.age=21 #给Cat添加属性
-Cat.a="a"  #给Cat添加属性
+cat3=Cat()
+
+cat1.a=19 #给cat1添加属性
+cat2.a=20 #给cat2添加属性
+Cat.a=21 #给Cat添加属性
+
 #print("%s的年龄是%d"%(cat1.name,cat1.age))
 cat1.eat()
-cat1.introduce()
-cat2.introduce()
-print(cat2.a)
+cat1.introduce2() #相当于 cat1.intraduce2(cat1)
+cat2.introduce2()
+cat3.introduce2()
 
