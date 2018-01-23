0cfb8c1a0a96dd6dddf9dd190d6984fed15f3ac4
Lmbp <leichen070@gmail.com>
2018-01-23 10:23:52 +0800

    对象只初始化一次

--- day11/05-只初始化一次对象.py
+++ day11/05-只初始化一次对象.py
@@ -1,13 +1,19 @@
 #coding=utf-8
 class Dog(object):
     __dog=None
-    def __new__(cls,x): #cls是Dog类,该方法在构造对象时会第一个被调用，用于创建对象，并返回所创建的对象
+    def __new__(cls,x):
         if(cls.__dog==None):
             cls.__dog=super().__new__(cls)
+            #cls.__dog.x=x
         return cls.__dog
 
+    __c=None
     def __init__(self,x):
-        self.x=x
+        #pass
+        if(self.__c==None):
+            Dog.__c=x
+            self.x=x
+            
     def __str__(self):
         return "对象的描述信息%s"%self.x
 
