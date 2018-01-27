4ac24b7119f1046ee726775cd51a4fcf1066faab
L <leichen070@gmail.com>
2018-01-27 22:15:08 +0800

    异常2

--- day11/06-异常.py
+++ day11/06-异常.py
@@ -4,8 +4,12 @@
 try:
     可能跑出异常的代码。。。
     。。。
-except 异常类型:
+except (异常类型1,异常类型2) as e:
     异常处理代码。。。
+else:
+    没有异常才会执行
+finally:
+    不管有没有异常都会执行
 
 java格式：
 try{
@@ -17,6 +21,18 @@ try{
 }
 '''
 try:
-    print(a)
-except NameError: #except（除非），异常名称:NameError
-    print("出现NameError异常，对异常进行处理")
+    1/0
+    #print(a)
+    #open("a.txt")
+    print("-----1-----")
+except (FileNotFoundError,NameError) as e: #except（除非），异常类型:NameError
+    print("出现NameError异常，对异常进行处理",end="")
+    print("或出现FileNotFoundError异常，对异常进行处理")
+    print(e)
+except (Exception) as e:
+    print("全部的异常都可以用这个来捕获%s"%e)
+else:
+    print("没有异常才会执行")
+finally:
+    print("不管有没有异常都执行")
+print("-----2-----")
