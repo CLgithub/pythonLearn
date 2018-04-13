f4df635686ff70f4a2cc46feca105e7b4830a4f3
Lmbp <leichen070@gmail.com>
2018-04-13 19:36:24 +0800

    几种集合定义比较

---
+++ day6/12-几种集合定义总结.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+
+#tuple 元组
+tuple1=(5,6,'cat','dog')
+print(tuple1)
+
+#list列表
+list1=[1,3,6,2,'a','bc','8']
+print(list1)
+
+#set 集合
+set1={5,6,'cat','dog'}
+print(set1)
+
+#dict 字典
+dict1={5:'aa','cat':'cute','dog':'furry'}
+print(dict1)
